af3e897c931c8722ddcc201ad08cd5f69e5eef5c
Emmapr123 <emmapriesteer@hotmail.com>
2021-03-04 11:58:44 +0000

    random dice rol

--- lib/dice_roll.rb
+++ lib/dice_roll.rb
@@ -1,5 +1,5 @@
 class Dice
   def roll_dice
-
+    (1..6).to_a.sample
   end
 end
--- spec/dice_role_spec.rb
+++ spec/dice_role_spec.rb
@@ -1,9 +1,13 @@
 require "dice_roll"
 
 describe Dice do
-  describe "roll dice" do
+  describe "roll_dice" do
     it  "rolls dice" do
       expect(subject).to respond_to :roll_dice
+    end
+
+    it 'gives a number between 1 and 6' do
+      expect(subject.roll_dice).to be_between(1, 6)
     end
   end
 end
