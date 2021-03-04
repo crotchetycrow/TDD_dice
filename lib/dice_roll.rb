e40787e011a843df28d9022cea148b6f4ee6fec2
Emmapr123 <emmapriesteer@hotmail.com>
2021-03-04 12:20:31 +0000

    dice game finished

--- lib/dice_roll.rb
+++ lib/dice_roll.rb
@@ -1,9 +1,25 @@
 class Dice
+  attr_reader :dice_amount
+
+  def initialize
+    @dice_amount = 1
+    @rolled_dice = []
+  end
+
   def roll_dice
     (1..6).to_a.sample
+  end
+
+  def save_dice
+    @rolled_dice << roll_dice
   end
 
   def amount_of_dice
     3
   end
+
+  def number_of_rolls
+    amount_of_dice * roll_dice
+  end
+
 end
--- spec/dice_role_spec.rb
+++ spec/dice_role_spec.rb
@@ -18,4 +18,20 @@ describe Dice do
       expect(subject.amount_of_dice).to eq 3
     end
   end
+
+  describe "save_dice" do
+    it 'should see save_dice' do
+      expect(subject).to respond_to :save_dice
+    end
+  end
+
+  describe 'number_of_rolls' do
+    it 'sees function number_of_rolls' do
+      expect(subject).to respond_to :number_of_rolls
+    end
+    it 'rolls three dice' do
+      expect(subject.number_of_rolls).to be_between(3, 18)
+    end
+  end
+  
 end
